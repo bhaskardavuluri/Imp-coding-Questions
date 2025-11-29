0d684c4e0238fc33d6e0037d4ef95fdd2bd75c5b
bhaskardavuluri <bhaskardavuluri9949@gmail.com>
2025-11-29 22:58:01 +0530

    Update Test.java

--- Reverse of String/Test.java
+++ Reverse of String/Test.java
@@ -1,7 +1,7 @@
 public class Test{
     public static void main(String[] args) {
         String s1="rahul";
-        String s2=new StringBuilder(s1).reverse().toString();
+        String s2=new  StringBuilder(s1).reverse().toString();
         System.out.println(s2);
     }
 }
